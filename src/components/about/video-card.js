0ecc20d5e390d511730ceef1b59513c38ac09aa0
Marc Redemske <marc.redemske@gmail.com>
2018-10-17 17:25:14 +0200

    fix(about): add all video links
    See #1

--- src/components/about/video-card.js
+++ src/components/about/video-card.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
-export default ({ image, text }) => (
+export default ({ image, text, link }) => (
   <div className="c-video-card">
-    <img className="c-video-card__thumbnail" src={image} />
+    <a
+      className="text-box__link"
+      target="_blank"
+      rel="noopener
+      noreferrer"
+      href={link}
+    >
+      <img className="c-video-card__thumbnail" src={image} />
+    </a>
     <p className="c-video-card__text">{text}</p>
   </div>
 )
--- src/components/about/video-wall.js
+++ src/components/about/video-wall.js
@@ -4,17 +4,23 @@ import VideoCard from './video-card'
 import work from '../../static/work.png'
 import offsite from '../../static/offsite.png'
 import teamevent from '../../static/teamevent.png'
+import links from './video-links.json'
 
 export default () => (
   <NamespacesConsumer ns={'about'}>
     {t => (
       <div className="c-video-wall">
-        {[work, offsite, teamevent].map((image, i) => {
+        {[
+          [work, links.work],
+          [offsite, links.offsite],
+          [teamevent, links.teamevent],
+        ].map(([image, link], i) => {
           return (
             <VideoCard
               key={i.toString()}
               text={t(`life.${i + 1}.text`)}
               image={image}
+              link={link}
             />
           )
         })}
