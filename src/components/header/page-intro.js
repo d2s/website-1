1a84d52792c3f779f5cc46487994bf1e450c7e0d
Marc Redemske <marc.redemske@gmail.com>
2018-10-23 14:54:23 +0200

    fix: from component to pure function
    maybe this fixes the wrong header
    
    See: #1

--- src/components/header/page-intro.js
+++ src/components/header/page-intro.js
@@ -6,34 +6,32 @@ import Link from 'gatsby-link'
 
 const component = 'page-intro'
 
-export default class PageIntro extends Component {
-  render() {
-    const page = getCurrentPage()
-    const color = page == 'tech-hiring' ? 'blue' : 'yellow'
-    const target = page == 'index' ? 'tech-hiring' : ''
-    console.log(page, color, target)
-    return (
-      <NamespacesConsumer ns={page}>
-        {t => (
-          <div className="page-intro">
-            {/* <div className="page-intro__note">Note</div> */}
-            <h1 className="page-intro__headline">
-              {t(`${component}.headline1`)}
-              <br />
-              <b>{t(`${component}.headline2`)}</b>
-            </h1>
-            <p className="page-intro__tagline">{t(`${component}.tagline`)}</p>
-            <span className="page-intro__button-bar">
-              <a href={links[page]} className={`button button--${color}`}>
-                {t(`${component}.button1`)}
-              </a>
-              <Link to={getLinkTo(target)} className="button button--link">
-                {t(`${component}.button2`)}
-              </Link>
-            </span>
-          </div>
-        )}
-      </NamespacesConsumer>
-    )
-  }
+export default () => {
+  const page = getCurrentPage()
+  const color = page == 'tech-hiring' ? 'blue' : 'yellow'
+  const target = page == 'index' ? 'tech-hiring' : ''
+  console.log(page, color, target)
+  return (
+    <NamespacesConsumer ns={page}>
+      {t => (
+        <div className="page-intro">
+          {/* <div className="page-intro__note">Note</div> */}
+          <h1 className="page-intro__headline">
+            {t(`${component}.headline1`)}
+            <br />
+            <b>{t(`${component}.headline2`)}</b>
+          </h1>
+          <p className="page-intro__tagline">{t(`${component}.tagline`)}</p>
+          <span className="page-intro__button-bar">
+            <a href={links[page]} className={`button button--${color}`}>
+              {t(`${component}.button1`)}
+            </a>
+            <Link to={getLinkTo(target)} className="button button--link">
+              {t(`${component}.button2`)}
+            </Link>
+          </span>
+        </div>
+      )}
+    </NamespacesConsumer>
+  )
 }
