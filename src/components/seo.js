1d36e9f5abe2ca5e201f9032a8b60128d48ec5f8
Marc Redemske <marc.redemske@gmail.com>
2018-10-24 13:21:40 +0200

    feat(seo): add more lang properties
    See #1

--- src/components/seo.js
+++ src/components/seo.js
@@ -6,16 +6,27 @@ import { getLang } from '../utils/i18n'
 import { withPrefix } from 'gatsby'
 
 export default ({ page }) => {
+  const lang = getLang()
   let image = 'default-og.png'
   if (page === 'about' || page === 'community') {
     image = `${page}-og.jpg`
   }
+  const langs = ['en', 'de', 'nl'].filter(l => l !== lang)
   return (
     <NamespacesConsumer ns={'seo'}>
       {t => (
         <Helmet>
-          <html lang={getLang()} />
+          <html lang={lang} />
           <title>{t(`${page}.title`)}</title>
+          {langs.map(l => (
+            <link
+              rel="alternate"
+              href={`http://www.honeypot.io/${l}/${
+                page === 'index' ? '' : page
+              }`}
+              hrefLang={l}
+            />
+          ))}
           <meta name="description" content={t(`${page}.description`)} />
           <meta property="og:type" content="website" />
           <meta property="og:title" content={t(`${page}.og-title`)} />
