4e5cebfd0b5c26f81fa29c2b605c476658ae4a8e
Marc Redemske <marc.redemske@gmail.com>
2018-10-24 10:53:00 +0200

    feat(seo): set open graph tags
    See #1

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react'
 import Helmet from 'react-helmet'
 import { NamespacesConsumer } from 'react-i18next'
 import getCurrentPage from '../utils/page'
+import { withPrefix } from 'gatsby'
 
 export default ({ page }) => {
+  let image = 'default-og.png'
+  if (page === 'about' || page === 'community') {
+    image = `${page}-og.jpg`
+  }
   return (
     <NamespacesConsumer ns={'seo'}>
       {t => (
         <Helmet>
           <title>{t(`${page}.title`)}</title>
           <meta name="description" content={t(`${page}.description`)} />
+          <meta name="og:title" content={t(`${page}.og-title`)} />
+          <meta name="og:description" content={t(`${page}.og-description`)} />
+          <meta name="og:image" content={withPrefix(image)} />
+          <meta name="og:image:alt" content={t(`${page}.og-image-alt`)} />
         </Helmet>
       )}
     </NamespacesConsumer>
